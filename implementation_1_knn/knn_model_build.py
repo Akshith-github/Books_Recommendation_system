39922f3717056d8f0c83d3a418673ee9d18c3ddb
Akshith <akshithnukala9@gmail.com>
2021-04-22 00:08:05 +0530

    building and saving nearest neighbours model

---
+++ implementation_1_knn/knn_model_build.py
@@ -0,0 +1,53 @@
+from scipy.sparse import csr_matrix
+from sklearn.neighbors import NearestNeighbors
+import pandas as pd,os,pickle
+try:
+    from sklearn.externals import joblib
+except:
+    import joblib
+else:
+    print("action to import joblib failed ")
+from random import randint
+
+#dataset 
+base_loc = os.path.join(os.path.dirname(__file__), os.path.join('data'))
+us_canada_user_rating_pivot = pd.read_csv(base_loc+"\\us_canada_user_rating_pivot.csv")
+us_canada_user_rating_pivot.set_index("bookTitle",inplace = True)
+us_canada_user_rating_matrix = csr_matrix(us_canada_user_rating_pivot.values)
+#demo test_case
+def test_model_output(model_knn:NearestNeighbors):# if __name__ == '__main__':
+    query_index = randint(0,us_canada_user_rating_pivot.shape[0]-1)#np.random.choice(us_canada_user_rating_pivot.shape[0])
+    # print(query_index,us_canada_user_rating_pivot.shape)
+    distances, indices = model_knn.kneighbors(us_canada_user_rating_pivot.iloc[query_index,:].values.reshape(1, -1), n_neighbors = 6)
+    for i in range(0, len(distances.flatten())):
+        if i == 0:
+            print('Recommendations for {0}:\n'.format(us_canada_user_rating_pivot.index[query_index]))
+        else:
+            print('{0}: {1}, with distance of {2}:'.format(i, us_canada_user_rating_pivot.index[indices.flatten()[i]], distances.flatten()[i]))
+
+def build_knn_model():
+    #build model
+    model_knn = NearestNeighbors(metric = 'cosine', algorithm = 'brute')
+    model_knn.fit(us_canada_user_rating_matrix)
+    test_model_output(model_knn)
+    return model_knn
+
+model_knn = build_knn_model()
+# saving model 
+
+# Its important to use binary mode
+# 
+model_loc = os.path.join(os.path.dirname(__file__), os.path.join('models')) 
+knnPickle = open(model_loc+'\\knnpickle_file', 'wb') 
+# source, destination 
+pickle.dump(model_knn, knnPickle)                      
+knnPickle.close()
+# load the model from disk
+loaded_model = pickle.load(open(model_loc+'\\knnpickle_file', 'rb'))
+#test the models output
+test_model_output(loaded_model)
+
+## save using joblib library
+joblib.dump(model_knn , model_loc+'\\model_knn.pkl')
+modelknn_loaded = joblib.load( model_loc+'\\model_knn.pkl' , mmap_mode ='r')
+test_model_output(modelknn_loaded)
